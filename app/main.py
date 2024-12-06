797c22eb6f2429c9f047e6245fa930ab4d18296e
Zhadiger <98839309+Zhadi-1-s@users.noreply.github.com>
2024-12-06 17:07:09 +0500

    поменял пару импортов

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
-from routers import public_api
+from .routers import public_api, local_db
 
 app = FastAPI(title="Test Task API")
 
 
 app.include_router(public_api.router, prefix="/api/v1/public", tags=["Public API"])
-# app.include_router(local_db.router, prefix="/api/v1/local", tags=["Local DB"])
+app.include_router(local_db.router, prefix="/api/v1/local", tags=["Local DB"])
